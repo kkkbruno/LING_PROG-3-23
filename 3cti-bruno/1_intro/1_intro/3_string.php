19bb79a44933515e772ecd9cb4e529701481a704
Nanti <125514477+kkkbruno@users.noreply.github.com>
2023-03-08 21:05:33 -0300

    Delete 3cti-bruno/1_intro/1_intro directory

--- 3cti-bruno/1_intro/1_intro/3_string.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$a = "Mundo!";
-echo "Olá, $a"; //aspas duplas - permite concatenar variáveis
-echo "<br>";
-echo 'Olá, $a'; //aspas simples - não permite concatenar variáveis
-
-?>
--- 3cti-bruno/1_intro/1_intro/5_atribuicao.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $a = 1; //A variável $a é igual a 1
-    echo $a. "<hr>";
-    
-    $a += 2; // Somamos 2 ao valor da $a;
-    echo $a. "<hr>";
-    
-?>
